5d8b0869d8a14ff40e577a592d58cdc1fa796a45
very-amused <very-amused@pm.me>
2019-12-06 06:36:42 -0500

    Add time and date validation

--- pages/create.vue
+++ pages/create.vue
@@ -3,11 +3,11 @@
     v-card(v-bind="card" class="mt-5")
       v-card-title(:class="card.title.class") Create a Plan
       v-divider
-      v-form
+      v-form(lazy-validation)
         v-col
           v-text-field(label="Title" :color="color" required)
           v-row
-            v-text-field(label="Time" hint="If AM or PM is not specified, this time will be interpreted as 24-hour." v-model="time.value" :error-messages="timeErrors" :color="color" required @change="validateTime")
+            v-text-field(label="Time" hint="If AM or PM is not specified, this time will be interpreted as 24-hour." v-model="time.value" :value="time.value" :error-messages="timeErrors" :color="color" required @change="validateTimeFormat")
             v-text-field(label="Date" :color="color" readonly required :value="date" append-icon="fa-calendar-alt" @click:append="toggleDatePicker")
             v-dialog(v-model="showDatePicker" max-width=500)
               v-date-picker(:color="color" v-model="date" :min="today")
@@ -19,15 +19,16 @@
 
 <script>
 export default {
-  data: () => {
+  data () {
     return {
       showDatePicker: false,
       date: null,
       time: {
         value: null,
-        format: null,
+        isTwentyFourHour: null,
         isValid: true
       },
+      timeDateObject: null,
 
       card: {
         class: 'card',
@@ -48,16 +49,33 @@ export default {
   computed: {
     today () {
       const d = new Date();
-      d.setDate(d.getDate() - 1);
+      d.setHours(0);
+      d.setMinutes(0);
+      d.setSeconds(0);
       return d.toISOString();
+      // Returns the current date at midnight (for purposes of the minimum date allowed on the date picker)
     },
     timeErrors () {
       if (!this.time.isValid) {
-        return 'Invalid time';
+        return 'Invalid time format.';
       }
-      else {
-        return null;
+      // Ensure timeDateObject exists before calling the getTime method on it
+      else if (this.timeDateObject) {
+        if (this.timeDateObject.getTime() <= new Date().getTime()) {
+          return 'This time has already elapsed.';
+        }
       }
+      return null;
+    }
+  },
+
+  watch: {
+    // Update the date object (contains information on both time and calendar date)
+    date () {
+      this.parseDateObject();
+    },
+    'time.value' () {
+      this.parseDateObject();
     }
   },
 
@@ -65,22 +83,49 @@ export default {
     toggleDatePicker () {
       this.showDatePicker = !this.showDatePicker;
     },
-    validateTime () {
+    validateTimeFormat () {
       // 12 hour time validation
-      const twelveHourRegex = new RegExp(/(\d|[01]\d):([012345]\d) ([AaPp][Mm])/);
+      const twelveHourRegex = new RegExp(/(\d|[01]\d):([012345]\d) ([AaPp][Mm])$/);
       // 24 hour time validation
-      const twentyFourHourRegex = new RegExp(/(\d|[01]\d|2[0123]):([012345]\d)/);
+      const twentyFourHourRegex = new RegExp(/(\d|[01]\d|2[0123]):([012345]\d)$/);
+      // Test the provided time against the regex
       if (twelveHourRegex.test(this.time.value)) {
         this.time.isValid = true;
-        this.time.format = '12H';
+        this.time.value = this.time.value.match(twelveHourRegex)[0];
+        this.time.isTwentyFourHour = false;
       }
       else if (twentyFourHourRegex.test(this.time.value)) {
         this.time.isValid = true;
-        this.time.format = '24H';
+        this.time.value = this.time.value.match(twentyFourHourRegex)[0];
+        this.time.isTwentyFourHour = true;
       }
       else {
         // Adjust time error if the time is invalid
         this.time.isValid = false;
+        this.time.format = null;
+      }
+    },
+    parseDateObject () {
+      // Return if the time or date don't exist or are invalid
+      if (!(this.time.value && this.time.isValid && this.date)) {
+        return;
+      }
+      // Parse year, month, and date
+      const splitDate = this.date.split('-');
+      const [year, month, date] = [...splitDate.map(i => parseInt(i))];
+      // Split the time for parsing
+      const splitTime = this.time.value.split(' ')[0].split(':');
+      // Parse hours and minutes
+      const [hours, minutes] = [...splitTime.map(i => parseInt(i))];
+      this.timeDateObject = new Date(year, month - 1, date, hours, minutes);
+      // Adjust time if 12 hour time format is being used
+      if (!this.time.isTwentyFourHour) {
+        if (hours === 12 && this.time.value.toLowerCase().includes('am')) {
+          this.timeDateObject.setHours(hours - 12);
+        }
+        else if (hours !== 12 && this.time.value.toLowerCase().includes('pm')) {
+          this.timeDateObject.setHours(hours + 12);
+        }
       }
     }
   }

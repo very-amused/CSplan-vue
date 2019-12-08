ca8eebd40a9eb9cb779252bf0ea1acee788c9c30
very-amused <very-amused@pm.me>
2019-12-07 23:09:10 -0500

    Cleaned up plan creation form
    Also created the first version of submit method, used to submit the form's data to the CSplan API

--- pages/create.vue
+++ pages/create.vue
@@ -3,16 +3,16 @@
     v-card(v-bind="card" class="mt-5")
       v-card-title(:class="card.title.class") Create a Plan
       v-divider
-      v-form(lazy-validation)
+      v-form
         v-col
-          v-text-field(label="Title" :color="color" required)
+          v-text-field(label="Title" :color="color" v-model="title.value"  :error-messages="titleErrors" required)
           v-row
-            v-text-field(label="Time" hint="If AM or PM is not specified, this time will be interpreted as 24-hour." v-model="time.value" :value="time.value" :error-messages="errors" :color="color" required @change="validateTimeFormat")
+            v-text-field(label="Time" hint="If AM or PM is not specified, this time will be interpreted as 24-hour." v-model="time.value" :error-messages="timeErrors" :color="color" required @change="validateTimeFormat")
             v-text-field(label="Date" :color="color" readonly required :value="date" append-icon="fa-calendar-alt" @click:append="toggleDatePicker")
             v-dialog(v-model="showDatePicker" max-width=500)
               v-date-picker(:color="color" v-model="date" :min="today")
                 v-icon(class="close-icon" color="white" @click="toggleDatePicker") fas fa-times
-          v-textarea(label="Description (optional)" outlined no-resize rows="3" :color="color" class="mb-0")
+          v-textarea(label="Description (optional)" outlined no-resize rows="3" maxlength=2000 counter v-model="description" :color="color" class="mb-0")
         v-col(align="center")
           v-btn(large :color="`${color} white--text`" class="mt-0" @click="submit") Create
 </template>
@@ -22,13 +22,21 @@ export default {
   data () {
     return {
       showDatePicker: false,
-      date: null,
+      // Data concerning the plan creation form, in order of top to bottom, left to right
+      title: {
+        value: null,
+        /* Is the field empty AND the user has tried submitting the form,
+        thus prompting the error linked to the truthiness of this variable */
+        isEmptyAfterSubmit: false
+      },
       time: {
         value: null,
-        isTwentyFourHour: null,
-        isValidFormat: true
+        isValidFormat: true,
+        isTwentyFourHour: null
       },
-      timeDateObject: null,
+      date: null,
+      timestamp: null, // Unix timestamp combining date and time
+      description: null,
 
       card: {
         class: 'card',
@@ -47,15 +55,21 @@ export default {
   },
 
   computed: {
+    // Returns the current date at midnight (for purposes of the minimum date allowed on the date picker)
     today () {
       const d = new Date();
       d.setHours(0);
       d.setMinutes(0);
       d.setSeconds(0);
       return d.toISOString();
-      // Returns the current date at midnight (for purposes of the minimum date allowed on the date picker)
     },
-    errors () {
+    titleErrors () {
+      if (this.title.isEmptyAfterSubmit) {
+        return 'A title is required before this plan can be created';
+      }
+      return null;
+    },
+    timeErrors () {
       if (!this.time.isValidFormat) {
         return 'Invalid time format.';
       }
@@ -72,10 +86,10 @@ export default {
   watch: {
     // Update the date object (contains information on both time and calendar date)
     date () {
-      this.parseTimeDateObject();
+      this.parseTimestamp();
     },
     'time.value' () {
-      this.parseTimeDateObject();
+      this.parseTimestamp();
     }
   },
 
@@ -102,10 +116,9 @@ export default {
       else {
         // Adjust time error if the time is invalid
         this.time.isValidFormat = false;
-        this.time.format = null;
       }
     },
-    parseTimeDateObject () {
+    parseTimestamp () {
       // Return if the time or date don't exist or are invalid
       if (!(this.time.value && this.time.isValidFormat && this.date)) {
         return;
@@ -117,26 +130,40 @@ export default {
       const splitTime = this.time.value.split(' ')[0].split(':');
       // Parse hours and minutes
       const [hours, minutes] = [...splitTime.map(i => parseInt(i))];
-      this.timeDateObject = new Date(year, month - 1, date, hours, minutes);
+      const dateObj = new Date(year, month - 1, date, hours, minutes);
       // Adjust time if 12 hour time format is being used
       if (!this.time.isTwentyFourHour) {
         if (hours === 12 && this.time.value.toLowerCase().includes('am')) {
-          this.timeDateObject.setHours(hours - 12);
+          dateObj.setHours(hours - 12);
         }
         else if (hours !== 12 && this.time.value.toLowerCase().includes('pm')) {
-          this.timeDateObject.setHours(hours + 12);
+          dateObj.setHours(hours + 12);
         }
       }
+      // Set the timestamp in Unix epoch time
+      this.timestamp = dateObj.getTime();
     },
-    submit () {
-      /* Only submit the form if 3 conditions are met:
-      1. There has been a time set
-      2. There has been a date set
-      3. There are no errors in the form */
-      if (!(this.time.value && this.date && !this.errors)) {
+    async submit () {
+      // If the user hasn't set a title yet, prompt them to
+      if (!this.title) {
+        this.title.isEmptyAfterSubmit = true;
+        return;
+      }
+      // Don't submit the form if it's missing a valid timestamp or there are errors with the time
+      else if (!(this.timestamp && !this.timeErrors)) {
         return;
       }
       // Post with axios
+      const response = await this.$axios.$post('https://07eef260.ngrok.io/API/create-plan', {
+        title: this.title.value,
+        timestamp: this.timestamp,
+        description: this.description
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(response);
     }
   }
 };

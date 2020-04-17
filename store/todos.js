bcfb89f0ed3fda177deaf027922d27a7f3c5471c
very-amused <very-amused@pm.me>
2020-04-17 15:40:58 -0400

    Add API interaction for more todo state changes

--- store/todos.js
+++ store/todos.js
@@ -38,9 +38,9 @@ export const actions = {
       url: '/v1/todos/ids'
     });
     const data = response.data.data;
-    let todos = await this.$dexie.todos.toArray();
+    const todos = await this.$dexie.todos.toArray();
     const privateKey = await importPrivateKey(rootState.user.keys.privateKey);
-    data.forEach(async (identifier) => {
+    for (const identifier of data) {
       if (!todos.find(list => (list.id === identifier.id &&
         list.checksum === identifier.meta.checksum))) {
         const response = await this.$axios({
@@ -59,38 +59,49 @@ export const actions = {
         }
         const cryptoKey = await unwrapSymmetricKey(doc.meta.cryptoKey, privateKey);
 
-        const encryptedItems = doc.relationships.items.data.map((item) => {
-          return {
-            ...item.attributes,
-            completed: item.attributes.completed === 'true'
-          };
-        });
+        const encryptedItems = doc.relationships.items.data.map(item => item.attributes);
         const { title, items } = await deepDecrypt({
           title: doc.attributes.title,
           items: encryptedItems
         }, cryptoKey);
 
+        const mappedItems = items.map((item) => {
+          return {
+            ...item,
+            completed: item.completed === 'true'
+          };
+        });
+
         const list = {
           id: doc.id,
           checksum: doc.meta.checksum,
           title,
-          items,
+          items: mappedItems,
           cryptoKey
         };
 
         await this.$dexie.todos.put(list);
-        todos = await this.$dexie.todos.toArray();
       }
-    });
+    };
 
-    todos.forEach((list) => {
+    const updatedTodos = await this.$dexie.todos.toArray();
+    for (const list of updatedTodos) {
       commit('addList', list);
-    });
+    }
   },
   async addList ({ commit, dispatch, rootState }, list) {
     const publicKey = await importPublicKey(rootState.user.keys.publicKey);
     const { usable, exported } = await genSymmetricKey(publicKey);
     const encrypted = await deepEncrypt(list, usable.symmetricKey);
+    const mappedItems = encrypted.items.map((item) => {
+      return {
+        type: 'todo-item',
+        attributes: {
+          ...item
+        }
+      };
+    });
+
     const { data } = await this.$axios({
       method: 'POST',
       url: '/v1/todos',
@@ -102,14 +113,7 @@ export const actions = {
           },
           relationships: {
             items: {
-              data: encrypted.items.map((item) => {
-                return {
-                  type: 'todo-item',
-                  attributes: {
-                    ...item
-                  }
-                };
-              })
+              data: mappedItems
             }
           },
           meta: {
@@ -153,16 +157,40 @@ export const actions = {
     await this.$dexie.todos.put(list);
     commit('addItem', { id, item });
   },
-  async toggleCompletion ({ commit, state, dispatch }, { id, itemIndex }) {
+  async toggleCompletion ({ commit, state, rootState }, { id, itemIndex }) {
     const index = state.findIndex(list => list.id === id);
     const completed = !state[index].items[itemIndex].completed;
+    const encryptedCompleted = await deepEncrypt(completed, state[index].cryptoKey);
+
+    await this.$axios({
+      method: 'PATCH',
+      url: `/v1/todos/${id}/items/${itemIndex}`,
+      data: {
+        data: {
+          type: 'todo-item',
+          attributes: {
+            completed: encryptedCompleted
+          }
+        }
+      }
+    });
+
     const list = await this.$dexie.todos.get(id);
     list.items[itemIndex].completed = completed;
     await this.$dexie.todos.put(list);
 
     commit('setItemCompletion', { id, itemIndex, completed });
   },
-  removeItem ({ commit, dispatch }, { id, itemIndex }) {
+  async removeItem ({ commit, dispatch }, { id, itemIndex }) {
+    await this.$axios({
+      method: 'DELETE',
+      url: `/v1/todos/${id}/items/${itemIndex}`
+    });
+
+    const list = await this.$dexie.todos.get(id);
+    list.items.splice(itemIndex, 1);
+    await this.$dexie.todos.put(list);
+
     commit('removeItem', { id, itemIndex });
   }
 };
